e319116450b13b9d6486850ceee471b79208c4c8
chengsijia <xiaocheng911@163.com>
2020-05-04 12:34:20 +0800

    “搭建学烘焙框架

---
+++ src/views/cookStudy/Add.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Add extends Component {
+    render() {
+        return (
+            <div>
+                <h1>上传食谱</h1>
+                <h1>上传作品</h1>
+            </div>
+        )
+    }
+}
