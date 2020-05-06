e616c049fa06b13d17eaeb252e02442f2acb6340
chengsijia <xiaocheng911@163.com>
2020-05-06 14:54:44 +0800

    完善学烘焙推荐课程数据渲染

--- src/components/cookStudy/CookClass.jsx
+++ src/components/cookStudy/CookClass.jsx
@@ -5,13 +5,37 @@ export default class CookClass extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            category: []
+            category: [],
+            advise:[]
         };
     }
     render() {
         return (
             <div>
                 <div className={style.main}>
+                    {
+                        <div className={style.cla}>
+                            <h2>
+                                推荐课程
+                            </h2>
+                            <div className={style.xxx}>
+                                <div>
+                                    {
+                                    this.state.advise.map(vv => (
+                                        <section onClick={()=>{
+                                            this.props.props.history.push(`/lesson/${vv.courseId}`);
+                                            console.log(111,this.props)
+                                        }} key={vv.courseId}>
+                                            <img src={vv.coverImage} alt="" />
+                                            <i>{vv.buyNum < 1000 ? vv.buyNum : "1000+"}  在学</i>
+                                            <h3>{vv.title}</h3>
+                                        </section>
+                                    ))
+                                }
+                                </div>
+                            </div>
+                        </div>
+                    }
                     {
                         this.state.category.map(v => (
                             <div key={v.categoryId} className={style.cla}>
@@ -21,10 +45,10 @@ export default class CookClass extends Component {
                                 <div className={style.xxx}>
                                     <div>
                                         {
-                                            v.item.map(vv => (
-                                                <section onClick={()=>{
+                                            v.item.map((vv, i) => (
+                                                <section onClick={() => {
                                                     this.props.props.history.push(`/lesson/${vv.educationCourseId}`);
-                                                    console.log(111,this.props)
+                                                    console.log(111, this.props)
                                                 }} key={vv.categoryItemId}>
                                                     <img src={vv.verticalImages[0]} alt="" />
                                                     <i>{vv.buyNum < 1000 ? vv.buyNum : "1000+"}  在学</i>
@@ -44,6 +68,7 @@ export default class CookClass extends Component {
     async componentDidMount() {
         console.log(111111);
         const { data } = await this.$axios.get("/hbb/education/getIndexByWeb?_t=1588577321865");
+        data.category.splice(0, 1);
         const category = data.category;
         console.log(category)
         this.setState({
@@ -51,19 +76,10 @@ export default class CookClass extends Component {
         });
 
         const data2 = await this.$axios.get("/hbb/recommend/getRandContent?_t=1588577321861&type=3&pageSize=10")
-        const advise = data2.data;
+        const advise = data2.data.data;
         console.log(advise);
-        // const cate = this.state.category.splice(0,1);
-        // const arr = [
-        //     advise,
-        //     ...this.state.category
-        // ];
-        // console.log(arr)
-        // this.setState({
-        //     category:[
-        //         advise,
-        //         ...this.state.category.splice(0,1)
-        //     ]
-        // })
+        this.setState({
+            advise
+        })
     }
 }
