609dda1863632bc720b5bea90351f6b6858dd2c2
chengsijia <xiaocheng911@163.com>
2020-05-15 16:58:53 +0800

    优化

--- src/views/cookStudy/index/Video.jsx
+++ src/views/cookStudy/index/Video.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 // import CookClass from '../../../components/cookStudy/CookClass'
 import RootRouter from './../../../components/common/RootRouter';
-// import { Carousel, WingBlank } from 'antd-mobile';
-import 'antd-mobile/lib/carousel/style/css'; 
+import { Carousel } from 'antd-mobile';
+import 'antd-mobile/lib/carousel/style/css';
 // 引入样式
 import style from "../css/video.module.scss"
 // 引入图片
@@ -34,55 +34,64 @@ export default class Video extends Component {
             <div>
                 <div className={style.videoCla}>
                     <div className={style.middleCla}>
-                        <NavLink exact activeClassName={style.Aactive} to={"/video"}>
+                        <NavLink exact activeClassName={style.Aactive} to={"/video"} onClick={() => {
+                            this.setState({
+                                claOpen: false
+                            })
+                        }}>
                             推荐
                             <i></i>
                         </NavLink>
                         {
                             this.state.cla.map((v, i) => (
-                                <NavLink activeClassName={style.Aactive} to={"/video/c" + i + "?" + v.categoryId} key={v.categoryId}>
+                                <NavLink activeClassName={style.Aactive} to={"/video/c" + i + "?" + v.categoryId} key={v.categoryId} onClick={() => {
+                                    console.log(1111);
+                                    this.setState({
+                                        claOpen: true
+                                    })
+                                }}>
                                     {v.title}
                                     <i></i>
                                 </NavLink>
                             ))
                         }
                     </div>
-                    <img onClick={()=>{
+                    <img onClick={() => {
                         this.props.history.push("/search");
                     }} className={style.ss} src={search} alt="" />
                 </div>
-                    {/* <Carousel
-                        autoplay={true}
-                        infinite={true}
-                        
-                    >
-                        {this.state.data.map((val, i) => (
-                            <a
-                                key={val}
-                                href='http://localhost:3000/lesson/10589'
-                                style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
-                            >
-                                <img
-                                    src={this.state.imgList[i]}
-                                    alt=""
-                                    style={{ width: '100%', verticalAlign: 'top' }}
-                                    onLoad={() => {
-                                        // fire window resize event to change height
-                                        window.dispatchEvent(new Event('resize'));
-                                        this.setState({ imgHeight: 'auto' });
-                                    }}
-                                />
-                            </a>
-                        ))}
-                    </Carousel> 
-                    */}
+                <Carousel
+                    className={this.state.claOpen ? style.hide : style.show}
+                    autoplay={true}
+                    infinite={true}
+                >
+                    {this.state.data.map((val, i) => (
+                        <a
+                            key={val}
+                            href='http://localhost:3000/lesson/10589'
+                            style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
+                        >
+                            <img
+                                src={this.state.imgList[i]}
+                                alt=""
+                                style={{ width: '100%', verticalAlign: 'top' }}
+                                onLoad={() => {
+                                    // fire window resize event to change height
+                                    window.dispatchEvent(new Event('resize'));
+                                    this.setState({ imgHeight: 'auto' });
+                                }}
+                            />
+                        </a>
+                    ))}
+                </Carousel>
+
                 <RootRouter router={this.props.childrens}></RootRouter>
             </div>
 
         )
     }
     async componentDidMount() {
-       
+
         const { data } = await this.$axios.get("/hbb/index/getByType?_t=1588671929336&type=11");
         console.log(data);
         this.setState({
